bc2abbd937290ec0c1fe5a7bb409044254ab222d
bhattaramsindhura603 <sindhurabhattaram@gmail.com>
2017-01-25 10:01:15 +0530

    Update Main.java

--- WebCrawler/src/WebCrawler/Main.java
+++ WebCrawler/src/WebCrawler/Main.java
@@ -1,6 +1,7 @@
 package WebCrawler;
 
 
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -15,63 +16,61 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 public class Main{
 	
+	 
+	  
+	 public static ArrayList<String> searchCriteria=new ArrayList<String>();
+	 public static String keyToTest;
+	
 	public static void main(String[] args) throws IOException {
+		
+		String url = "http://www.mit.edu/";
+		keyToTest="research";
         //Validate.isTrue(args.length == 1, "usage: supply url to fetch");
-    String url = "http://www.mit.edu/";
-	System.setProperty("http.proxyHost", "proxy.india.cypress.com");
-	System.setProperty("http.proxyPort", "8080");
-    Document doc = Jsoup.connect("http://www.mit.edu/").get();
-    String title = doc.title();  
-    System.out.println("title is: " + title);  
-    BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-   int rank = 0;
-   System.out.println("Enter  Search "
-   		+ "Keyword");
-   String key="molecule";
-if(key !=null &&!( key.equals(""))){
-   System.out.println(key);
-    Map<String,ArrayList<String> >mapObject =new HashMap<String,ArrayList<String>>();
-  
-    ArrayList<String> searchCriteria=new ArrayList<String>();
-
-    Elements links = doc.select("a[href]");
-    for (Element link : links) {
-    	if(link.text().contains(key)){
-    		
-    		searchCriteria.add(link.attr("abs:href"));
-     System.out.println(link.attr("abs:href"));
-    	}
-    	
-    	
-    }
-    mapObject.put(key,searchCriteria);
-    key=key.toUpperCase();
-
-    switch(key){
-    case "RESEARCH":
-    	rank=1;
-    	break;
-    case "TECHNOLOGY":
-       rank=2;
-       break;
-    case "FACILITIES":
-        rank=3;
-        break;
-    }
- 
-    
- System.out.println("rank is "+rank);
- System.out.println("Pages containing the specific keyword");
- 
- searchCriteria.forEach(System.out::println);
-    	
+		System.setProperty("http.proxyHost", "proxy.india.cypress.com");
+		System.setProperty("http.proxyPort", "8080");
+		
+		    processAction(url,searchCriteria,keyToTest);
     
     
-    }
+    
    
 	}
+
+	public static void processAction(String url, ArrayList<String> searchCriteria2, String keyToTest2) throws IOException {
+		// TODO Auto-generated method stubSystem.setProperty("http.proxyHost", "proxy.india.cypress.com");
+		System.setProperty("http.proxyHost", "proxy.india.cypress.com");
+		System.setProperty("http.proxyPort", "8080");
+	    Document doc = Jsoup.connect(url).get();
+	    String title = doc.title();  
+	    System.out.println("title is: " + title);  
+	    
+	   int rank = 0;
+	  
+
+	   System.out.println(keyToTest2);
+	   
+
+	    Elements links = doc.select("a[href]");
+	    for (Element link : links) {
+	    	if(link.text().contains(keyToTest2)){
+	    	
+	    		searchCriteria.add(link.attr("abs:href"));
+	    		
+	     System.out.println(link.attr("abs:href"));
+	    	}
+	    }
+
+	    
+	    
+	 
+	    
+	 System.out.println("rank is "+searchCriteria.size());
+	 System.out.println("Pages containing the specific keyword");
+	 
+	 searchCriteria.forEach(System.out::println);
+		
+	}
 }
-	
 
 
 
